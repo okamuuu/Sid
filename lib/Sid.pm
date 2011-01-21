8d3e0062c4bcd1925279d6c1f9e0a3c55eab3f21
okamuuu <okamuuu@gmail.com>
2011-01-21 20:17:37 +0900

    initial commit

---
+++ lib/Sid.pm
@@ -0,0 +1,32 @@
+package Sid;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Sid -
+
+=head1 SYNOPSIS
+
+  use Sid;
+
+=head1 DESCRIPTION
+
+Sid is
+
+=head1 AUTHOR
+
+okamura E<lt>okamuuu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
