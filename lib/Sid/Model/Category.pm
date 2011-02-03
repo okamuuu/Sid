09e83bdee256e3fc236e19ec9d662dab78c83da7
okamuuu <okamuuu@gmail.com>
2011-02-03 22:18:29 +0900

    generate more simple html. refs 17

--- lib/Sid/Model/Category.pm
+++ lib/Sid/Model/Category.pm
@@ -1,10 +1,12 @@
 package Sid::Model::Category;
+use strict;
+use warnings;
 use Class::Accessor::Lite 0.05 (
     new => 1,
-    ro  => [qw/id name archives_ref/],
+    ro  => [qw/id name articles_ref/],
 );
 
-sub archives { @{ $_[0]->{archives_ref} } }
+sub articles { @{ $_[0]->{articles_ref} } }
 
 1;
 
